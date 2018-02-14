2945407ad25dd7a7437df26d8c15046032e71534
Memet RIZA <memetriza@Memets-iMac.local>
2018-02-14 17:19:50 +0200

    Added login screen

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Memet RIZA on 14/02/2018.
+//  Copyright © 2018 Memet RIZA. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Memet RIZA on 14/02/2018.
+//  Copyright © 2018 Memet RIZA. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
